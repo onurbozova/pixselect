2c5af960d9463918d36dcb3c15852c561829ab76
onurbozova <bozova@itu.edu.tr>
2019-03-20 10:19:52 +0300

    Updates

---
+++ precache-manifest.387201a89a5311301a4735951fc37842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4d3fcff241121fd51b",
+    "url": "/pixselect/static/css/main.7b01aae6.chunk.css"
+  },
+  {
+    "revision": "4e4d3fcff241121fd51b",
+    "url": "/pixselect/static/js/main.c02fbb84.chunk.js"
+  },
+  {
+    "revision": "b73f3a7ca6ab5c722f97",
+    "url": "/pixselect/static/js/runtime~main.6b0d38b0.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/pixselect/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pixselect/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4f98e225c81ff967666c089e351252ea",
+    "url": "/pixselect/index.html"
+  }
+];
